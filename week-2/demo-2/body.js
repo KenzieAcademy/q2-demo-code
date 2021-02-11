48a0af7efb0058c34a504985c2f97a5d50e05c12
Peter Mayor <petermayor@Peters-MacBook-Pro.local>
2021-02-11 16:28:44 -0500

    minor updates/add conditional rendering

--- week-2/demo-2/body.js
+++ week-2/demo-2/body.js
@@ -1,5 +1,13 @@
 class Body extends React.Component {
-  state = { rate: 10 };
+  constructor(props) {
+    super(props);
+    this.state = { rate: 10 };
+    // this.increase = this.increase.bind(this)
+  }
+
+  componentDidMount() {
+    console.log("Ran component did mount");
+  }
 
   increase = () => {
     if (this.state.rate < 20) {
@@ -14,7 +22,9 @@ class Body extends React.Component {
   };
 
   render = () => {
+    console.log(`Ran render method`, this);
     const { age, species, image } = this.props.person;
+    // this.props.person = {age: 100, species: "Cool guy", image: "./header.webp"}
     const { rate } = this.state;
 
     return (
@@ -27,6 +37,11 @@ class Body extends React.Component {
           <button onClick={this.increase}>+</button>
           <button onClick={this.decrease}>-</button>
         </div>
+        {this.state.rate > 10 ? (
+          <h1>Wow that rating is so high</h1>
+        ) : (
+          <h1>Wow that rating stinks</h1>
+        )}
       </div>
     );
   };
--- week-2/demo-2/footer.js
+++ week-2/demo-2/footer.js
@@ -1,7 +1,12 @@
-class Footer extends React.Component {
-    render() {
-        return (
-            <h1>Footer</h1>
-        )
-    }
+// class Footer extends React.Component {
+//   render() {
+//     return <h1>Footer</h1>;
+//   }
+// }
+
+function Footer(props) {
+  return <h1>Footer</h1>;
 }
+
+
+
