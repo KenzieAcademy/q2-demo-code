46531325a6825a1b0e7c05e140b3ed11a480e9ad
Peter Mayor <petem14@gmail.com>
2021-03-11 14:14:41 -0500

    end of 3/11 review, add useContext

--- week-6/review-demo-1/src/store/store.js
+++ week-6/review-demo-1/src/store/store.js
@@ -1,16 +1,21 @@
+// create actions here
 export const INCREMENT = "INCREMENT";
 export const DECREMENT = "DECREMENT";
 export const TOGGLE_COLOR = "TOGGLE_COLOR";
 export const CREATE = "CREATE";
 
 export function reducer(state, action) {
+  //copy the counterlist
   const newCounterList = [...state.counterList];
+  // copy the clicked counter object from our state
   const clickedCounterObject = { ...newCounterList[action.payload] };
   switch (action.type) {
     case INCREMENT:
       clickedCounterObject.count = clickedCounterObject.count + 1;
       newCounterList[action.payload] = clickedCounterObject;
       return {
+        // don't forget to spread the old state when returning the new state 
+        // so that we don't clear out our other state properties
         ...state,
         counterList: newCounterList,
       };
@@ -32,6 +37,7 @@ export function reducer(state, action) {
     case CREATE:
       return {
         ...state,
+        // an example of updating a list of objects with a new object
         counterList: [
           ...state.counterList,
           { count: 0, id: state.counterList.length, color: "red" },
