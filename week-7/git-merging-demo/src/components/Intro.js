2c4ef50b969ad2087b23ed8e34960b1b9707a710
tjhindman <thind131@gmail.com>
2021-03-17 13:07:52 -0400

    added Intro component

---
+++ week-7/git-merging-demo/src/components/Intro.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div>
+      <h1>Meet your Instructors!</h1>
+      <ul>
+        <li>Hi I'm Meagan!</li>
+        <li>Hi I'm Gabby!</li>
+        <li>Hi I'm Mike!</li>
+        <li>Hi I'm Mia!</li>
+        <li>Hi I'm Gwen!</li>
+        <li>Hi I'm Peter!</li>
+        <li>Hi I'm TJ!</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Intro;
